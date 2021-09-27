8cc1c95dcca3ba344ae8fdc94ef3c20fc3403e5a
MikePonce-UwU <mvallecillo101@gmail.com>
2021-09-27 17:31:53 -0600

    cambiar estilo del navbar

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,11 +1,12 @@
 <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-    <div class="container-fluid">
-        <a class="navbar-brand ps-3" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
-        </a>
-        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!">
-            <i class="fas fa-bars"></i>
-        </button>
+    <a class="navbar-brand ps-3" href="{{ url('/') }}">
+        <i class="far fa-gem"></i> {{ config('app.name', 'Laravel') }}
+    </a>
+    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!">
+        <i class="fas fa-bars"></i>
+    </button>
+    <div class="px-5">
+
         <!-- Left Side Of Navbar -->
         <ul class="navbar-nav me-auto">
 
